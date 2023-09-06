dc3c6e4eee3335eadc137199ee470e2d3ca8d9c0
Jan Vlnas <jan.vlnas@mews.com>
2023-09-06 23:57:07 +0200

    Handle enum serialization

--- Meows/MeowsApi/Program.cs
+++ Meows/MeowsApi/Program.cs
@@ -1,13 +1,21 @@
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(opts =>
+    {
+        var enumConverter = new JsonStringEnumConverter();
+        opts.JsonSerializerOptions.Converters.Add(enumConverter);
+    });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
