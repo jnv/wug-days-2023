a390e76061efd34379bb1d33d688677ecb70ad48
Jan Vlnas <jan.vlnas@mews.com>
2023-09-07 20:57:53 +0200

    Required properties

--- Meows/MeowsApi/Dto/Pet.cs
+++ Meows/MeowsApi/Dto/Pet.cs
@@ -1,9 +1,16 @@
-﻿namespace MeowsApi.Dto;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeowsApi.Dto;
 
 public record Pet
 {
+    [Required]
     public string Id { get; init; }
+
+    [Required]
     public string Name { get; init; }
+
+    [Required]
     public PetKind Kind { get; init; }
 
     public string OwnerEmail { get; init; }
--- Meows/MeowsApi/Dto/Reservation.cs
+++ Meows/MeowsApi/Dto/Reservation.cs
@@ -1,9 +1,18 @@
-﻿namespace MeowsApi.Dto;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeowsApi.Dto;
 
 public record Reservation
 {
+    [Required]
     public string Id { get; init; }
+
+    [Required]
     public DateTime StartAt { get; init; }
+
+    [Required]
     public DateTime EndAt { get; init; }
+
+    [Required]
     public IEnumerable<string> GuestsIds { get; init; }
 }
