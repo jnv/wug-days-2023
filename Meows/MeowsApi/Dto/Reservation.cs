c3f7554a608e6a8162c81642d818e51a40b8e357
Jan Vlnas <jan.vlnas@mews.com>
2023-09-08 12:49:03 +0200

    Annotate reservation

--- Meows/MeowsApi/Dto/Reservation.cs
+++ Meows/MeowsApi/Dto/Reservation.cs
@@ -1,19 +1,27 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace MeowsApi.Dto;
 
+[SwaggerSchema(
+    Description = "Represents a reservation",
+    Required = new[] { "Id", "StartAt", "EndAt", "GuestsId" }
+)]
 public record Reservation
 {
-    [Required]
+    [SwaggerSchema("The reservation identifier", ReadOnly = true)]
     public string Id { get; init; }
 
     [Required]
+    [SwaggerSchema("Start time of reservation")]
     public DateTime StartAt { get; init; }
 
     [Required]
+    [SwaggerSchema("End time of reservation")]
     public DateTime EndAt { get; init; }
 
     [Required]
     [MinLength(1)]
+    [SwaggerSchema("List of guests")]
     public IEnumerable<string> GuestsIds { get; init; }
 }
